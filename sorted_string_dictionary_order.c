92f46f0174dee723099cd7e30723db87627b01b6
Abhishek Sharma <92991046+abhisheksharma27948@users.noreply.github.com>
2022-09-07 10:35:22 +0530

    Create sorted_string_dictionary_order.c

---
+++ sorted_string_dictionary_order.c
@@ -0,0 +1,32 @@
+// Take 5 names as i/p and sort them in dictionary order.
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+	int i,j,n;
+	char str[100][100],s[100];
+	printf("Enter number of names :\n");
+	scanf("%d",&n);
+	printf("Enter names in any order:\n");
+	for(i=0;i<n;i++)
+	{
+		scanf("%s",str[i]);
+	}
+	for(i=0;i<n;i++)
+	{
+		for(j=i+1;j<n;j++)
+		{
+			if(strcmp(str[i],str[j])>0)
+			{
+            	strcpy(s,str[i]);
+            	strcpy(str[i],str[j]);
+            	strcpy(str[j],s);
+        	}
+	    }
+	}
+	printf("\nAfter Sorting : \n");
+	for(i=0;i<n;i++)
+	{
+		printf("%s\n",str[i]);
+	}
+}
